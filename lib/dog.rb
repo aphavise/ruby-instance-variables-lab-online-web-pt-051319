2d56ba2fa12a5b71a0cc7f2c8425e2ce83c8db3b
ANDREW PHAVISETH <andrewphaviseth@gmail.com>
2019-06-01 20:06:06 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,26 @@
-class Dog 
+# class Dog 
   
+#   def name=(dog_name)
+#     this_dogs_name = dog_name
+#   end 
+  
+  
+#   def name 
+#     this_dogs_name
+#   end
+  
+# end
+
+
+class Dog
   def name=(dog_name)
     this_dogs_name = dog_name
-  end 
-  
-  
-  def name 
+  end
+ 
+  def name
     this_dogs_name
   end
-  
 end
-
 
 lassie = Dog.new 
 lassie.name = "Lassie"
